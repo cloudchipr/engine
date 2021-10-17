6461e4fa479e44bd8b9e5f1880a45732668170d3
Apple <apple@Apples-iMac.local>
2021-10-17 14:44:46 +0400

    Clean Elb, Nlb, Alb

--- src/adapters/aws-shell-engine-adapter.ts
+++ src/adapters/aws-shell-engine-adapter.ts
@@ -1,7 +1,6 @@
 import { EngineInterface } from './engine-interface'
 import * as policies from '../policy.json'
 import { Ebs } from '../domain/types/aws/ebs'
-import { EngineResponse } from '../engine-response'
 import { EngineRequest } from '../engine-request'
 import { C7nFilterBuilder } from '../filters/c7n-filter-builder'
 import { C7nExecutor } from '../c7n-executor'
@@ -22,54 +21,23 @@ export class AWSShellEngineAdapter<Type> implements EngineInterface<Type> {
     const command = request.command.getValue()
     const subCommand = request.subCommand.getValue()
 
-    const methodName = AWSShellEngineAdapter.getMethodName(command, subCommand)
-    this.validateRequest(methodName)
+    const generateResponseMethodName = AWSShellEngineAdapter.getResponseMethodName(subCommand)
+    this.validateRequest(generateResponseMethodName)
 
-    return (this as any)[methodName](request)
-  }
+    const policyName = `${subCommand}-${command}`
+    this.validatePolicyName(policyName)
 
-  private collectEbs (request: EngineRequest): EngineResponse {
-    return this.generateEbsResponse(this.collect(request, 'ebs-collect'))
-  }
-
-  private cleanEbs (request: EngineRequest): EngineResponse {
-    return this.generateEbsResponse(this.collect(request, 'ebs-clean'))
-  }
-
-  private collectEc2 (request: EngineRequest): EngineResponse {
-    return this.generateEc2Response(this.collect(request, 'ec2-collect'))
-  }
-
-  private cleanEc2 (request: EngineRequest): EngineResponse {
-    return this.generateEc2Response(this.collect(request, 'ec2-clean'))
-  }
-
-  private collectElb (request: EngineRequest): EngineResponse {
-    return this.generateElbResponse(this.collect(request, 'elb-collect'))
-  }
-
-  private collectNlb (request: EngineRequest): EngineResponse {
-    return this.generateElbResponse(this.collect(request, 'nlb-collect'))
-  }
-
-  private collectAlb (request: EngineRequest): EngineResponse {
-    return this.generateElbResponse(this.collect(request, 'alb-collect'))
-  }
-
-  private collect (request: EngineRequest, policyName: string): any {
-    if (!(policyName in policies)) {
-      throw new Error(`Invalid policy name provided: ${policyName}`)
-    }
     // @ts-ignore
     const policy: any = Object.assign({}, policies[policyName])
     policy.policies[0].filters.push(request.parameter.filter.build(new C7nFilterBuilder()))
 
     // execute custodian command and return response
-    return this.custodianExecutor.execute(
+    const response = this.custodianExecutor.execute(
       request.configuration,
       policy,
       policyName
     )
+    return (this as any)[generateResponseMethodName](response)
   }
 
   private validateRequest (name: string) {
@@ -78,8 +46,14 @@ export class AWSShellEngineAdapter<Type> implements EngineInterface<Type> {
     }
   }
 
-  private static getMethodName (command: string, subCommand: string): string {
-    return command + AWSShellEngineAdapter.capitalizeFirstLetter(subCommand)
+  private validatePolicyName (policy: string) {
+    if (!(policy in policies)) {
+      throw new Error(`Invalid policy name provided: ${policy}`)
+    }
+  }
+
+  private static getResponseMethodName (subCommand: string): string {
+    return `generate${AWSShellEngineAdapter.capitalizeFirstLetter(subCommand)}Response`
   }
 
   private static capitalizeFirstLetter (str: string): string {
@@ -143,6 +117,52 @@ export class AWSShellEngineAdapter<Type> implements EngineInterface<Type> {
   }
 
   private generateElbResponse (
+    responseJson: any
+  ): Response<Type> {
+    return new Response<Type>(
+      responseJson.sort((a: any, b: any) => (a.CreatedTime > b.CreatedTime) ? 1 : ((b.CreatedTime > a.CreatedTime) ? -1 : 0))
+        .map(
+          (elbResponseItemJson: {
+            DNSName: string;
+            CreatedTime: string;
+            Price: string;
+            Tags: any[];
+          }) => {
+            return new Elb(
+              elbResponseItemJson.DNSName,
+              DateTimeHelper.getAge(elbResponseItemJson.CreatedTime),
+              'not implemented',
+              TagsHelper.getNameTagValue(elbResponseItemJson.Tags)
+            )
+          }
+        )
+    )
+  }
+
+  private generateNlbResponse (
+    responseJson: any
+  ): Response<Type> {
+    return new Response<Type>(
+      responseJson.sort((a: any, b: any) => (a.CreatedTime > b.CreatedTime) ? 1 : ((b.CreatedTime > a.CreatedTime) ? -1 : 0))
+        .map(
+          (elbResponseItemJson: {
+            DNSName: string;
+            CreatedTime: string;
+            Price: string;
+            Tags: any[];
+          }) => {
+            return new Elb(
+              elbResponseItemJson.DNSName,
+              DateTimeHelper.getAge(elbResponseItemJson.CreatedTime),
+              'not implemented',
+              TagsHelper.getNameTagValue(elbResponseItemJson.Tags)
+            )
+          }
+        )
+    )
+  }
+
+  private generateAlbResponse (
     responseJson: any
   ): Response<Type> {
     return new Response<Type>(
