bc75230a7378e816dfc1dd77696ec8b12fb3cf90
Apple <apple@Apples-iMac.local>
2021-12-04 01:28:35 +0400

    Move inside try

--- src/c7n-executor.ts
+++ src/c7n-executor.ts
@@ -23,79 +23,79 @@ export class C7nExecutor {
     ) {
       const id: string = `${policyName}-${v4()}`
       const timestamp: string = Date.now().toString()
-      const dir: string = `./tmp/c7r/${timestamp}/${id}/`
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true })
-      }
-      const policyPath: string = `${dir}policy.yaml`
-      fs.writeFileSync(policyPath, yaml.dump(policy), 'utf8')
-
-      let regionOptions = ''
-      if (regions) {
-        regionOptions = regions.map(region => ' --region ' + region).join(' ')
-      }
-
-      // validate all region request
-      const all = regions.find(region => region === 'all')
-      if (all !== undefined) {
-        throw new Error('Region all is not implemented yet')
-      }
-
-      let result: {
-          C8rRegion: string|undefined,
-          C8rAccount: string|undefined
-      }[] = []
-
-      let includeCurrentAccount = true
-      // use multi account, but not for the case when the accounts is 1 and it is current one
-      const useMultiAccount = accounts.length > 0 && !(accounts.length === 1 && currentAccount && accounts.includes(currentAccount))
-      if (useMultiAccount) {
-        if (regions.length === 0) {
-          regions.push('us-east-1') // default AWS region @todo must be changed later for other clouds
-        }
-
-        includeCurrentAccount = currentAccount !== undefined && accounts.includes(currentAccount)
-        // exclude current account from custodian anyway it will be fetch by signe cloud custodian
-        if (includeCurrentAccount) {
-          accounts = accounts.filter(a => a !== currentAccount)
-        }
-
-        // custodianOrg exists and executable
-        // @ts-ignore it is just for snake case
-        const accountsObject: {
-            accounts: {
-                accountId: string,
-                name: string,
-                regions: string[],
-                role: string
-            }[]
-        } = {}
-
-        // @ts-ignore it is just for snake case
-        accountsObject.accounts = accounts.map(id => {
-          return {
-            account_id: id,
-            name: id,
-            regions: regions,
-            role: `arn:aws:iam::${id}:role/OrganizationAccountAccessRole`
-          }
-        })
-
-        const accountsConfigFile: string = `${dir}accounts.yaml`
-        fs.writeFileSync(`${accountsConfigFile}`, yaml.dump(accountsObject), 'utf8')
-        const command = `${this.custodianOrg} run ${regionOptions} -c ${accountsConfigFile} -s ${dir}response-org  -u ${policyPath} --cache-period=0`
-
-        if (isDebugMode) {
-          DebugHelper.log(command)
-        }
-
-        execSync(
-          command,
-          { stdio: 'pipe' }
-        )
-      }
-
       try {
+        const dir: string = `./tmp/c7r/${timestamp}/${id}/`
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir, { recursive: true })
+        }
+        const policyPath: string = `${dir}policy.yaml`
+        fs.writeFileSync(policyPath, yaml.dump(policy), 'utf8')
+
+        let regionOptions = ''
+        if (regions) {
+          regionOptions = regions.map(region => ' --region ' + region).join(' ')
+        }
+
+        // validate all region request
+        const all = regions.find(region => region === 'all')
+        if (all !== undefined) {
+          throw new Error('Region all is not implemented yet')
+        }
+
+        let result: {
+            C8rRegion: string|undefined,
+            C8rAccount: string|undefined
+        }[] = []
+
+        let includeCurrentAccount = true
+        // use multi account, but not for the case when the accounts is 1 and it is current one
+        const useMultiAccount = accounts.length > 0 && !(accounts.length === 1 && currentAccount && accounts.includes(currentAccount))
+        if (useMultiAccount) {
+          if (regions.length === 0) {
+            regions.push('us-east-1') // default AWS region @todo must be changed later for other clouds
+          }
+
+          includeCurrentAccount = currentAccount !== undefined && accounts.includes(currentAccount)
+          // exclude current account from custodian anyway it will be fetch by signe cloud custodian
+          if (includeCurrentAccount) {
+            accounts = accounts.filter(a => a !== currentAccount)
+          }
+
+          // custodianOrg exists and executable
+          // @ts-ignore it is just for snake case
+          const accountsObject: {
+              accounts: {
+                  accountId: string,
+                  name: string,
+                  regions: string[],
+                  role: string
+              }[]
+          } = {}
+
+          // @ts-ignore it is just for snake case
+          accountsObject.accounts = accounts.map(id => {
+            return {
+              account_id: id,
+              name: id,
+              regions: regions,
+              role: `arn:aws:iam::${id}:role/OrganizationAccountAccessRole`
+            }
+          })
+
+          const accountsConfigFile: string = `${dir}accounts.yaml`
+          fs.writeFileSync(`${accountsConfigFile}`, yaml.dump(accountsObject), 'utf8')
+          const command = `${this.custodianOrg} run ${regionOptions} -c ${accountsConfigFile} -s ${dir}response-org  -u ${policyPath} --cache-period=0`
+
+          if (isDebugMode) {
+            DebugHelper.log(command)
+          }
+
+          execSync(
+            command,
+            { stdio: 'pipe' }
+          )
+        }
+
         if (includeCurrentAccount) {
           const command = `${this.custodian} run ${regionOptions} --output-dir=${dir}response  ${policyPath} --cache-period=0`
 
