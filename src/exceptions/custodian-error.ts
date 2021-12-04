3b7395ae72751fe06cc75250db4618e705069849
Apple <apple@Apples-iMac.local>
2021-12-05 02:07:38 +0400

    more options for the exception

--- src/exceptions/custodian-error.ts
+++ src/exceptions/custodian-error.ts
@@ -1,5 +1,7 @@
 export class CustodianError extends Error {
   private readonly id?: string
+  private readonly executionDetails?: string
+  private readonly timestamp: number
 
   constructor (message?: string, id?: string) {
     super(message)
@@ -9,9 +11,19 @@ export class CustodianError extends Error {
     }
 
     this.id = id
+    this.executionDetails = id !== undefined ? `./tmp/c7r/${id}/` : ''
+    this.timestamp = Date.now()
   }
 
   getId (): string | undefined {
     return this.id
+  }
+
+  getExecutionDetails (): string | undefined {
+    return this.executionDetails
+  }
+
+  getTimestamp (): number {
+    return this.timestamp
   }
 }
