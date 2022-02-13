6407e1e88fc398185242f4723d1dbf9726e74c1e
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2022-02-13 13:03:57 +0100

    Add elb tags type (#137)

--- src/helpers/tags-helper.ts
+++ src/helpers/tags-helper.ts
@@ -1,4 +1,6 @@
 import { Tag as Ec2Tag } from '@aws-sdk/client-ec2'
+import { Tag as ElbV3Tag } from '@aws-sdk/client-elastic-load-balancing'
+import { Tag as ElbV2Tag } from '@aws-sdk/client-elastic-load-balancing-v2'
 import { Tag as RdsTag } from '@aws-sdk/client-rds'
 import { Tag } from '../domain/tag'
 
@@ -7,7 +9,7 @@ export class TagsHelper {
     return tags?.find(tagObject => ['NAME', 'Name', 'name'].includes(tagObject.Key))?.Value ?? ''
   }
 
-  static formatTags (tags?: Ec2Tag[] | RdsTag[]): Tag[] {
+  static formatTags (tags?: (Ec2Tag | RdsTag | ElbV3Tag | ElbV2Tag)[]): Tag[] {
     return tags?.reduce((previousValue: Tag[], currentValue) => {
       if (currentValue.Key !== undefined && currentValue.Value !== undefined) {
         return [...previousValue, new Tag(currentValue.Key, currentValue.Value)]
