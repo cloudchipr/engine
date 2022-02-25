b52d032b58980235f4e7d0df1116063db294fe81
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2022-02-25 16:36:33 +0100

    Add more data to the response objects (#151)

--- src/domain/types/gcp/disks.ts
+++ src/domain/types/gcp/disks.ts
@@ -1,6 +1,14 @@
+import { Label } from './shared/label'
+
 export class Disks {
   constructor (
     readonly name: string,
+    readonly type?: string,
+    readonly status?: string,
+    readonly size?: string,
+    readonly age?: string,
     readonly zone?: string,
+    readonly pricePerMonth?: number,
+    readonly labels?: Label[]
   ) {}
 }
--- src/domain/types/gcp/eip.ts
+++ src/domain/types/gcp/eip.ts
@@ -1,7 +1,11 @@
+import { Label } from './shared/label'
+
 export class Eip {
   constructor (
     readonly ip: string,
     readonly name?: string,
-    readonly region?: string
+    readonly region?: string,
+    readonly pricePerMonth?: number,
+    readonly labels?: Label[]
   ) {}
 }
