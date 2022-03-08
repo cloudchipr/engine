f0a23ae7507aab10805822cc812e5be074c51a0c
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2022-03-08 13:18:43 +0100

    Revert a file

--- src/filters/filter-validator.ts
+++ src/filters/filter-validator.ts
@@ -18,8 +18,7 @@ export class FilterValidator {
 
     public validate (filters: Filters) {
       if (!filters.and) {
-        filters.and = [] // @todo remove this line and uncomment the next line once the filters work for GCP
-        // throw new Error('Filter validation failed : The root element must either be `and` or `or`')
+        throw new Error('Filter validation failed : The root element must either be `and` or `or`')
       }
 
       filters.and.forEach(filter => {
