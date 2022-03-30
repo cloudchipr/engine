447ca3b828c4e22df191f7a60a563b8f21e6aae6
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2022-03-30 21:30:52 +0200

    refactor gcp client

--- src/adapters/gcp/clients/gcp-base-client.ts
+++ src/adapters/gcp/clients/gcp-base-client.ts
@@ -1,16 +1,7 @@
 import { Response } from '../../../responses/response'
-import { CleanRequestResourceInterface } from '../../../request/clean/clean-request-resource-interface'
 
 export default class GcpBaseClient {
   async getAdditionalDataForFormattedCollectResponse<Type> (response: Response<Type>): Promise<Response<Type>> {
     return response
-  }
-
-  isCleanRequestValid (request: CleanRequestResourceInterface): boolean {
-    return Boolean(request.id)
-  }
-
-  getCleanCommands (request: CleanRequestResourceInterface): Promise<any> {
-    return new Promise(() => {})
   }
 }
