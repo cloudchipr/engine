96c6e8bcfb866067770582a36418e419ba082689
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2022-03-30 21:28:34 +0200

    remove eip

--- src/adapters/gcp/clients/gcp-eip-client.ts
+++ src/adapters/gcp/clients/gcp-eip-client.ts
@@ -5,6 +5,10 @@ import { StringHelper } from '../../../helpers/string-hepler'
 import { Eip } from '../../../domain/types/gcp/eip'
 import { Label } from '../../../domain/types/gcp/shared/label'
 import GcpBaseClient from './gcp-base-client'
+import { CleanRequestResourceInterface } from '../../../request/clean/clean-request-resource-interface'
+import {
+  CleanGcpLbEipMetadataInterface
+} from '../../../request/clean/clean-request-resource-metadata-interface'
 
 export default class GcpEipClient extends GcpBaseClient implements GcpClientInterface {
   getCollectCommands (regions: string[]): any[] {
@@ -14,6 +18,23 @@ export default class GcpEipClient extends GcpBaseClient implements GcpClientInte
     }
     promises.push(GcpEipClient.getGlobalAddressesClient().list({ project: 'cloud-test-340820' }))
     return promises
+  }
+
+  getCleanCommands (request: CleanRequestResourceInterface): Promise<any> {
+    const metadata = request.metadata as CleanGcpLbEipMetadataInterface
+    if (metadata.global) {
+      return GcpEipClient.getGlobalAddressesClient().delete({ address: request.id, project: 'cloud-test-340820' })
+    } else {
+      return GcpEipClient.getAddressesClient().delete({ address: request.id, region: metadata.region, project: 'cloud-test-340820' })
+    }
+  }
+
+  isCleanRequestValid (request: CleanRequestResourceInterface): boolean {
+    if (!('metadata' in request) || !request.metadata) {
+      return false
+    }
+    const metadata = request.metadata as CleanGcpLbEipMetadataInterface
+    return metadata.global || !!metadata.region
   }
 
   async formatCollectResponse<Type> (response: any[]): Promise<Response<Type>> {
