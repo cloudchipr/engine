55f1c852b81a29fe8e383c36238cbd38262c1a3c
Sergey Mirzoyan <sergey.mirzoyan1991@gmail.com>
2022-05-25 23:52:31 +0400

    Add LB attachment value

--- src/adapters/gcp/clients/gcp-client.ts
+++ src/adapters/gcp/clients/gcp-client.ts
@@ -29,21 +29,25 @@ export class GcpClient {
 
   async collectResources<Type> (): Promise<Response<Type>[]> {
     const authClient = await this.getAuthClient()
+    // get all needed resources
     const responses = await Promise.all([
       GcpDisksClient.collectAll(authClient, this.projectId),
       GcpVmClient.collectAll(authClient, this.projectId),
       GcpLbClient.collectAll(authClient, this.projectId),
       GcpEipClient.collectAll(authClient, this.projectId),
       GcpSqlClient.collectAll(authClient, this.projectId),
+      GcpLbClient.collectAllTargetPool(authClient, this.projectId),
       GcpCatalogClient.collectAllComputing(authClient),
       GcpCatalogClient.collectAllSql(authClient)
     ])
+    // set LBs attachment value
+    GcpLbClient.setAttachmentValue(responses[2].items as Lb[], responses[1].items as Vm[], responses[5])
+    // calculate prices
     await GcpPriceCalculator.putDisksPrices(responses[0].items as Disks[], authClient)
     await GcpPriceCalculator.putVmPrices(responses[1].items as Vm[], responses[0].items as Disks[], authClient)
     await GcpPriceCalculator.putLbPrices(responses[2].items as Lb[], authClient)
     await GcpPriceCalculator.putEipPrices(responses[3].items as Eip[], authClient)
     await GcpPriceCalculator.putSqlPrices(responses[4].items as Sql[], authClient)
-    await GcpVmClient.getMetrics(authClient, this.projectId, responses[1])
     return [responses[0], responses[1], responses[2], responses[3], responses[4]] as Response<Type>[]
   }
 
--- src/adapters/gcp/clients/gcp-lb-client.ts
+++ src/adapters/gcp/clients/gcp-lb-client.ts
@@ -5,6 +5,7 @@ import { Lb } from '../../../domain/types/gcp/lb'
 import { CleanRequestResourceInterface } from '../../../request/clean/clean-request-resource-interface'
 import { CleanGcpLbEipMetadataInterface } from '../../../request/clean/clean-request-resource-metadata-interface'
 import { google } from 'googleapis'
+import { Vm } from '../../../domain/types/gcp/vm'
 
 export class GcpLbClient {
   static async collectAll<Type> (auth: any, project: string): Promise<Response<Type>> {
@@ -16,15 +17,49 @@ export class GcpLbClient {
           data.push(new Lb(
             v.name || '',
             StringHelper.splitAndGetAtIndex(v.region || '', '/', -1) || '',
+            true,
             v.IPProtocol || '',
             !('region' in v),
             v.creationTimestamp || '',
+            v.target,
             Label.createInstances(v.labels || {})
           ))
         })
       }
     })
     return new Response<Type>(data)
+  }
+
+  static async collectAllTargetPool (auth: any, project: string): Promise<{[K: string]: string[]}> {
+    const data: {[K: string]: string[]} = {}
+    const result: any = await google.compute('v1').targetPools.aggregatedList({ auth, project })
+    Object.keys(result.data.items).forEach(key => {
+      if ('targetPools' in result.data.items[key] && Array.isArray(result.data.items[key].targetPools)) {
+        result.data.items[key].targetPools?.forEach((v: any) => {
+          for (const instance of (v.instances || [])) {
+            if (!(v.name in data)) {
+              data[v.name] = []
+            }
+            data[v.name].push(StringHelper.splitAndGetAtIndex(instance, '/', -1) || '')
+          }
+        })
+      }
+    })
+    return data
+  }
+
+  static setAttachmentValue (lb: Lb[], vm: Vm[], targetPools: {[K: string]: string[]}): void {
+    lb.forEach((l: Lb) => {
+      const targetType = StringHelper.splitAndGetAtIndex(l.target, '/', -2)
+      const targetName = StringHelper.splitAndGetAtIndex(l.target, '/', -1)
+      if (targetType === 'targetPools' && targetName !== undefined) {
+        if (targetName in targetPools) {
+          l.hasAttachments = targetPools[targetName].filter((s) => vm.filter((v: Vm) => v.name === s).length > 0).length > 0
+        } else {
+          l.hasAttachments = false
+        }
+      }
+    })
   }
 
   static clean (auth: any, project: string, request: CleanRequestResourceInterface): Promise<any> {
